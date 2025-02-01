3bdc8e909be0b1ce9300668f64a5caba8b90a514
Prem16-pro <premizz1652@gmail.com>
2025-02-01 13:05:54 +0530

    bitwise

--- Lect_5/bitwise_operators.cpp
+++ Lect_5/bitwise_operators.cpp
@@ -4,11 +4,17 @@ using namespace std;
 
 int main(int argc, char const *argv[])
 {
-    int a,b;
-    cin >> a >> b ;
+    int a=4,b=6;
     cout << "a&b : " << (a&b) << endl;
     cout << "a|b : " << (a|b) << endl;
     cout << "~a : " << ~a << endl;
     cout << "a^b : " << (a^b) << endl;
+
+    cout << (17>>1) << endl;
+    cout << (17>>2) << endl;
+
+    cout << (19<<1) << endl;
+    cout << (21<<2) << endl;
+
     return 0;
 }
